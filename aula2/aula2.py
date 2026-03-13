d27fb7b4657ab2f64293f44cd770f2908074e682
RodrigoSilva1989 <rodrigodomandela@hotmail.com>
2026-03-13 06:12:21 -0300

    feat: exercícios da segunda aula de python, dados inteiros, float, string e bollean

---
+++ aula2/aula2.py
@@ -0,0 +1,134 @@
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# valor1 = int(input("Digite um valor inteiro: "))
+# valor2 = int(input("Digite um segundo valor inteiro: "))
+
+# resultado = valor1 + valor2
+
+# print(f"A soma dos números foi de {resultado}")
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# valor1 = int(input("Digite um valor inteiro: "))
+
+# resultado = valor1 % 5
+
+# print(f"O resto da divisão por 5 é {resultado}")
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# valor1 = int(input("Digite um valor inteiro: "))
+# valor2 = int(input("Digite um segundo valor inteiro: "))
+
+# resultado = valor1 * valor2
+
+# print(f"A resultado da multiplicação é {resultado}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# valor1 = int(input("Digite um valor inteiro: "))
+# valor2 = int(input("Digite um segundo valor inteiro: "))
+
+# resultado = valor1 // valor2
+
+# print(f"A resultado da multiplicação é {resultado}")
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# valor1 = int(input("Digite um valor inteiro: "))
+
+# resultado = valor1 ** 2
+
+# print(f"A resultado do número ao quadrado é {resultado}")
+
+# #### Números de Ponto Flutuante (`float`)
+
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# valor1 = float(input("Digite um valor decimal: "))
+# valor2 = float(input("Digite um segundo valor decimal: "))
+
+# resultado = valor1 + valor2
+
+# print(f"A resultado da soma é {resultado}")
+
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# valor1 = float(input("Digite um valor decimal: "))
+# valor2 = float(input("Digite um segundo valor decimal: "))
+
+# resultado = (valor1 + valor2)/2
+
+# print(f"A resultado da média é {resultado}")
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# base = float(input("Digite um valor decimal: "))
+# expoente = float(input("Digite um segundo valor decimal: "))
+
+# resultado = base ** expoente
+
+# print(f"{base} elevado a {expoente} é igual a {resultado}")
+
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# (0 °C × 9/5) + 32
+# temperatura = float(input("Digite a temperatura em graus celsius: "))
+
+# resultado = temperatura * (9/5) + 32
+
+# print(f"A temperatura em Fahrenheit é {resultado}")
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# A = πr^2
+# raio = float(input("Digite o raio do círculo: "))
+
+# resultado = 3.14 * raio ** 2
+
+# print(f"A área do círculo é {resultado}")
+
+# #### Strings (`str`)
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# stg1 = input("Digite uma string: ")
+
+# resultado = stg1.upper()
+
+# print(f"String em maiúscula {resultado}")
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# stg1 = input("Digite uma string: ")
+
+# resultado = stg1.lower()
+
+# print(f"String em minúscula {resultado}")
+
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# stg1 = input("Digite uma frase: ")
+
+# resultado = stg1.strip()
+
+# print(f"Frase sem espaço em branco no início e final {resultado}")
+
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# stg1 = input("Digite uma data no formato dd/mm/yyyy: ")
+
+# resultado = stg1.split("/")
+
+# print(f"dia {resultado[0]}, mês {resultado[1]}, ano {resultado[2]}")
+
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+# stg1 = input("Digite uma string: ")
+# stg2 = input("Digite uma segunda string: ")
+
+# resultado = stg1 + stg2
+
+# print(f"O resultado da string concatenada é: {resultado}")
+
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+# #### try-except e if
+
+# 21: Conversor de Temperatura
+# 22: Verificador de Palíndromo
+# 23: Calculadora Simples
+# 24: Classificador de Números
+# 25: Conversão de Tipo com Validação
