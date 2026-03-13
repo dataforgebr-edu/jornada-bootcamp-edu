f5905a26379c39acb209ea517592c82f870e825b
RodrigoSilva1989 <rodrigodomandela@hotmail.com>
2026-03-13 05:24:46 -0300

    fix: correção das variáveis de entrada do usuário para incluir o tipo de dados

--- aula1/aula1.py
+++ aula1/aula1.py
@@ -1,5 +1,9 @@
-nome_usuario = input("Digite o seu nome: ")
-valor_salario = input("Digite o valor do seu salário: ")
-fator_bonus = input("Digite o fator de bônus: ")
+CONSTANCE_BONUS_FIXO = 1000
 
-print(f"{nome_usuario} seu salaário total foi de {1000 + (int(valor_salario) * float(fator_bonus))}")
+nome_usuario = input("Digite o seu nome: ")
+valor_salario = float(input("Digite o valor do seu salário: "))
+fator_bonus = float(input("Digite o fator de bônus: "))
+
+total_bonus = CONSTANCE_BONUS_FIXO + (valor_salario * fator_bonus)
+
+print(f"Olá, {nome_usuario} seu salaário total foi de {total_bonus}")
